18d55b0067d40e290943fa8a6db019212ecb5ce0
MahmoudAGawad <mahmoudsaleh272@gmail.com>
2015-01-30 21:14:38 +0200

    new Layout

--- src/ImageProcessing.java
+++ src/ImageProcessing.java
@@ -1,18 +1,25 @@
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.EventListener;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -25,41 +32,76 @@ import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class ImageProcessing extends JPanel {
+public class ImageProcessing extends JFrame {
 	private static final long serialVersionUID = 1L;
-
+	private static Dimension dimension = java.awt.Toolkit.getDefaultToolkit()
+			.getScreenSize();
 	private BufferedImage image;
 	private JPanel canvas;
-	private JScrollPane viewer;
-	private JMenu file;
-	private JMenuItem openImage;
-	private JToolBar toolBar;
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+	private JMenuItem exit;
+	private JPanel toolBar, east, west;
+	private JScrollPane sp;
+	private JButton chooseImage;
+	private ImageIcon icon, close;
 
 	public ImageProcessing() {
-		
-		try {
-			this.image = ImageIO.read(new File("example.jpg"));
-		} catch (IOException ex) {
-			JOptionPane.showMessageDialog(this, "Image not found!");
-		}
 
-		JButton chooseImage = new JButton("Choose an image");
+		this.setLocation(dimension.width / 10, dimension.height / 10);
+		this.setSize(dimension.width - 2 * this.getX(), dimension.height - 2
+				* this.getY());
+
+		Container pane = this.getContentPane();
+
+		pane.setLayout(new BorderLayout());
+
+		loadImage("example.jpg");
+		initGUI();
+
+		sp.setPreferredSize(new Dimension(this.getWidth(),
+				this.getHeight() - 50));
+		pane.add(sp, BorderLayout.CENTER);
+
+		toolBar.add(chooseImage);
+
+		pane.add(east, BorderLayout.EAST);
+		pane.add(west, BorderLayout.WEST);
+		pane.add(toolBar, BorderLayout.SOUTH);
+
+		menuBar.add(fileMenu);
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		fileMenu.add(exit);
+		pane.add(menuBar, BorderLayout.NORTH);
+	}
+
+	private void initGUI() {
+		icon = new ImageIcon(
+				((new ImageIcon("open-file-icon.png").getImage()
+						.getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH))));
+		close = new ImageIcon(
+				((new ImageIcon("close.png").getImage()
+						.getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH))));
+		menuBar = new JMenuBar();
+
+		this.addComponentListener(new ComponentAdapter() {
+			public void componentResized(ComponentEvent e) {
+				reshape();
+			}
+		});
+
+		chooseImage = new JButton("open", icon);
 		chooseImage.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser chooser = new JFileChooser();
 				FileNameExtensionFilter filter = new FileNameExtensionFilter(
-						"JPEG PNG BMP", "jpg","png","bmp");
+						"JPEG PNG BMP", "jpg", "png", "bmp");
 				chooser.setFileFilter(filter);
 				int returnVal = chooser.showOpenDialog(getParent());
 				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					try {
-						image = ImageIO.read(new File(chooser.getSelectedFile()
-								.getAbsolutePath()));
-					} catch (IOException e1) {
-						JOptionPane.showMessageDialog(canvas, "Image not found!");
-					}
+					loadImage(chooser.getSelectedFile().getAbsolutePath());
 				}
 			}
 		});
@@ -70,28 +112,57 @@ public class ImageProcessing extends JPanel {
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				g.drawImage(image, 0, 0, null);
+				if (image != null)
+					g.drawImage(image, 0, 0, null);
 			}
 		};
-		canvas.add(chooseImage);
-		canvas.setPreferredSize(new Dimension(image.getWidth(), image
-				.getHeight()));
-		JScrollPane sp = new JScrollPane(canvas);
-		setLayout(new BorderLayout());
-		add(sp, BorderLayout.CENTER);
 
+		sp = new JScrollPane(canvas);
+
+		toolBar = new JPanel();
+		east = new JPanel();
+		west = new JPanel();
+
+		fileMenu = new JMenu("File");
+		exit = new JMenuItem(close);
+		exit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+	}
+
+	private void loadImage(String path) {
+		try {
+			this.image = ImageIO.read(new File(path));
+			if (canvas != null)
+				canvas.repaint();
+		} catch (IOException ex) {
+			JOptionPane.showMessageDialog(this, "Image not found!");
+		}
 	}
 
 	public static void main(String[] args) {
-		JPanel p = new ImageProcessing();
-		JFrame f = new JFrame();
-		f.setContentPane(p);
-		f.setSize(800, 100);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);
-//		f.setLayout(new GridLayout(3, 1));
+
+		JFrame mainFrame = new ImageProcessing();
+
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
 
 	}
 
-	
+	private void reshape() {
+		int min = Math.max(this.getHeight() / 10, 70);
+		sp.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()
+				- min));
+		toolBar.setPreferredSize(new Dimension(this.getWidth() / 2, min));
+
+		if (canvas != null && image != null)
+			canvas.setPreferredSize(new Dimension(image.getWidth(), image
+					.getHeight()));
+
+	}
+
 }
